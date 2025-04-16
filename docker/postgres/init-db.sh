ed783b72d5db3c0ed570645f6c248235c3ad2025
Rafael César <88472536+rafaelfrodz@users.noreply.github.com>
2025-04-16 20:31:50 -0300

    Delete docker/postgres directory

--- docker/postgres/init-db.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -e
-
-# Create test database
-psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
-    CREATE DATABASE estoque_test_db;
-    GRANT ALL PRIVILEGES ON DATABASE estoque_test_db TO postgres;
-EOSQL
